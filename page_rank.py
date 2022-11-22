d57aeb634072ae12dd12ef70ac84ff50a8482079
Madhav Garg <c2064676@newcastle.ac.uk>
2022-11-22 11:35:57 +0000

    Updated load_graph to put nodes and targets in list and put them in a dictionary

--- page_rank.py
+++ page_rank.py
@@ -14,16 +14,45 @@ def load_graph(args):
     Returns:
     A dict mapling a URL (str) to a list of target URLs (str).
     """
+    node_list = []
+    used_node_list = []
+    target_list = []
+    dict1 = {}
     # Iterate through the file line by line
     for line in args.datafile:
         # And split each line into two URLs
         node, target = line.split()
-        raise RuntimeError("This function is not implemented yet.")
+        if node in node_list:
+            pass
+        else:
+            node_list.append(node)
+    # print(node_list)
+
+        for i in range(len(node_list)):
+            if node_list[i] in used_node_list:
+                pass
+            else:
+                target_list.append(target)
+                dict1 = {node :[target_list]}
+
+    # print(target_list)
+    # print(node_list)
+    print(dict1)
+
+
+
+            #     for i in range(len(target_list)):
+            #         dict1 = {node: target_list}
+            # print(dict1)
+            # if node in dict1.keys():
+            #     if target not in target_list:
+            #         target_list.append(target)
+            #         print(target_list)
 
 
 def print_stats(graph):
-        """Print number of nodes and edges in the given graph"""
-        raise RuntimeError("This function is not implemented yet.")
+    """Print number of nodes and edges in the given graph"""
+    raise RuntimeError("This function is not implemented yet.")
 
 
 def stochastic_page_rank(graph, args):
@@ -68,13 +97,12 @@ parser.add_argument('-r', '--repeats', type=int, default=1_000_000, help="number
 parser.add_argument('-s', '--steps', type=int, default=100, help="number of steps a walker takes")
 parser.add_argument('-n', '--number', type=int, default=20, help="number of results shown")
 
-
 if __name__ == '__main__':
     args = parser.parse_args()
     algorithm = distribution_page_rank if args.method == 'distribution' else stochastic_page_rank
 
     graph = load_graph(args)
-
+    print(graph)
     print_stats(graph)
 
     start = time.time()
